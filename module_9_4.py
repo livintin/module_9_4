45e31659c61a703424766350ee26279c36e9851b
livintin <natig.abdulov@gmail.com>
2024-10-15 14:25:34 +0700

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,33 @@
+from random import choice
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        for i in data_set:
+            with open(file_name, 'a', encoding='utf-8') as file:
+                file.write(str(i) + '\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
